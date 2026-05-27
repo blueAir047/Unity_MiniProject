75b5fbc795f7b80afba467b132245575733ccdd4
blue_Air <blueair0379@gmail.com>
2026-05-27 16:58:35 +0900

    [스크립트 추가]게임데이터 매니저 및 공격카드스크립트 추가

---
+++ Assets/Scripts/Fgo/Manager/FgoGameDataManager.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class FgoGameDataManager : MonoBehaviour
+{
+   public static FgoGameDataManager Instance {  get; private set; }
+
+    [Serializable]
+    private class SerializationWrapper<T>
+    {
+        public List<T> items;
+    }
+
+    public Dictionary<string, FgoCharacterData> CharacterDataList {  get; private set; }
+    public Dictionary<string, FgoCommandCardData> CommandCardDataList { get; private set; }
+
+
+    private void Awake()
+    {
+        if (Instance == null) 
+        { 
+            Instance = this;
+        }
+
+        LoadAll();
+    }
+
+    private Dictionary<string, T> LoadData<T>(string fileName) where T : FgoGameDataBase
+    {
+        string path = $"JsonOutput/{fileName}";
+        TextAsset textAsset = Resources.Load<TextAsset>(path);
+
+        if (textAsset == null )
+        {
+            return new Dictionary<string, T>();
+        }
+
+        try
+        {
+            string jsonString = textAsset.text;
+
+            string wrappedJson = "{\"items\":" + jsonString + "}";
+            SerializationWrapper<T> wrapper = JsonUtility.FromJson<SerializationWrapper<T>>(wrappedJson);
+            Dictionary<string, T> resultDictionary = new Dictionary<string, T>();
+
+            foreach (T item in wrapper.items)
+            {
+                resultDictionary.Add(item.Id, item);
+            }
+
+            return resultDictionary;
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError($"[JSON 로드 오류]{fileName}: {ex.Message }");
+        }
+        return new Dictionary<string, T>();
+    }
+
+    public void LoadAll()
+    {
+        CharacterDataList = LoadData<FgoCharacterData>("FgoCharacterData");
+        CommandCardDataList = LoadData<FgoCommandCardData>("FgoCommandCardData");
+    }
+
+    public FgoCharacterData GetCharacterData(string id)
+    {
+        if (CharacterDataList.ContainsKey(id))
+        {
+            return CharacterDataList[id];
+        }
+        return null;
+    }
+
+    public FgoCommandCardData GetCommandCardData(string id) 
+    { 
+        if (CommandCardDataList.ContainsKey(id))
+        {
+            return CommandCardDataList[id];
+        }
+        return null;
+    }
+}
---
+++ Assets/Scripts/Fgo/UI/FgoCommadCardUI.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FgoCommandCardUI : MonoBehaviour
+{
+    [Header("--- 카드 연결---")]
+    [SerializeField] private Button btn_Card;
+    [SerializeField] private Image img_CardIcon;
+
+    public FgoCommandCardData CurrentCardData { get; private set; }
+    public Action<FgoCommandCardUI> OnCardSelectedEvent;
+
+    public bool IsSelected { get; private set; }
+    private RectTransform _rectTransform;
+
+    private float _selectedOffsetY = 50f;
+
+
+    private void Awake()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+
+    }
+    private void Start()
+    {
+        if (btn_Card != null) 
+        {
+            btn_Card.onClick.AddListener(OnClickCard);
+        }
+    }
+
+    public void OnClickCard()
+    {
+        if (CurrentCardData == null)
+        {
+            return;
+        }
+
+        if (OnCardSelectedEvent != null)
+        {
+            OnCardSelectedEvent.Invoke(this);
+        }
+    }
+
+    public void InitCard(FgoCommandCardData cardData)
+    {
+        CurrentCardData = cardData;
+
+        SetSelectedState(false);
+
+        if (cardData.IconPath != "")
+        {
+            Sprite loadedSprite = Resources.Load<Sprite>("Images/" + cardData.IconPath);
+
+            if (loadedSprite != null && img_CardIcon != null)
+            {
+                img_CardIcon.sprite = loadedSprite;
+            }
+        }
+    }
+
+    public void SetSelectedState(bool isSelected) 
+    {
+        if (IsSelected == isSelected) return;
+
+        IsSelected = isSelected;
+
+        if (_rectTransform != null)
+        {
+            Vector2 newPos = _rectTransform.anchoredPosition;
+
+            if (isSelected)
+            {
+                newPos.y += _selectedOffsetY;
+            }
+
+            else
+            {
+                newPos.y -= _selectedOffsetY;
+            }
+
+            _rectTransform.anchoredPosition = newPos;
+        }
+
+    }
+}
