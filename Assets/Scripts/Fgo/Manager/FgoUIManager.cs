e3326cc3e2ff0ec5f57ae9953fe1cf0d83456832
blue_Air <blueair0379@gmail.com>
2026-05-27 16:59:24 +0900

    [스크립트 수정]스크립트 추가에 따른 UI매니저 및 배틀UI 코드추가

--- Assets/Scripts/Fgo/Manager/FgoUIManager.cs
+++ Assets/Scripts/Fgo/Manager/FgoUIManager.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
-using Cysharp.Threading.Tasks;
 
 public class FgoUIManager : MonoBehaviour
 {
--- Assets/Scripts/Fgo/UI/FgoBattleUI.cs
+++ Assets/Scripts/Fgo/UI/FgoBattleUI.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class FgoBattleUI : FgoUIBase
 {
@@ -7,14 +8,20 @@ public class FgoBattleUI : FgoUIBase
     [SerializeField] private GameObject commandArea;
     [SerializeField] private GameObject commandCardArea;
 
+
+
     [Header("----클릭할 버튼---")]
     [SerializeField] private Button btn_Attack;
     [SerializeField] private Button btn_BattleEnd;
 
+    [Header("----커맨드 카드---")]
+    [SerializeField] private FgoCommandCardUI[] cardSlots;
 
+    private List<FgoCommandCardData> _selectedCards = new List<FgoCommandCardData>();
     private void OnEnable()
     {
         ResetBattleUI();
+        _selectedCards.Clear();
     }
 
     private void Start()
@@ -42,6 +49,13 @@ public class FgoBattleUI : FgoUIBase
             bool isCardActive = commandCardArea.activeSelf;
 
             commandCardArea.SetActive(!isCardActive);
+
+            commandArea.SetActive(isCardActive);
+
+            if (!isCardActive)
+            {
+                SetupCommandCards();
+            }
         }
     }
 
@@ -53,5 +67,57 @@ public class FgoBattleUI : FgoUIBase
     private void OpenBattleEndPopup()
     {
         FgoUIManager.Instance.OpenUI(FgoUIRootType.PopupUI, FgoUIType.ChoiceBattleEndPopup);
+    }
+
+    private void OnCommandCardClicked(FgoCommandCardUI clickedCard)
+    {
+        if (clickedCard.IsSelected)
+        {
+            _selectedCards.Remove(clickedCard.CurrentCardData);
+            clickedCard.SetSelectedState(false);
+
+            return;
+        }
+
+        if (_selectedCards.Count >= 3) 
+        { 
+            return; 
+        }
+
+        _selectedCards.Add(clickedCard.CurrentCardData);
+        clickedCard.SetSelectedState(true);
+
+        if (_selectedCards.Count == 3) 
+        {
+            StartAttackPhase();
+        }
+    }
+
+    private void SetupCommandCards()
+    {
+        FgoCommandCardData busterData = FgoGameDataManager.Instance.GetCommandCardData("card_buster_01");
+
+        if (busterData == null) return;
+
+        _selectedCards.Clear();
+
+        foreach (FgoCommandCardUI card in cardSlots)
+        {
+            if (card != null)
+            {
+                card.gameObject.SetActive(true);
+                card.InitCard(busterData);
+
+                card.OnCardSelectedEvent -= OnCommandCardClicked;
+                card.OnCardSelectedEvent += OnCommandCardClicked;
+
+            }
+        }
+    }
+
+    private void StartAttackPhase()
+    {
+        if (commandCardArea != null) commandCardArea.SetActive(false);
+        if (commandArea != null) commandArea.SetActive(true);
     }
 }
