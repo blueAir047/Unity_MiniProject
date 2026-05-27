c9a9dabb0ad60b99e9cf5cd8941dc6731c21374b
blue_Air <blueair0379@gmail.com>
2026-05-27 14:19:20 +0900

    [클래스 추가]제이슨파일연동용 클래스

---
+++ Assets/Scripts/Fgo/OnlyC#/FgoGameData.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+[System.Serializable]
+public class FgoCharacterData : FgoGameDataBase
+{
+    public string Name;
+    public string ServantClass;
+    public string SkillList;
+}
+
+[System.Serializable]
+public class FgoCommandCardData : FgoGameDataBase
+{
+    public string CardName;
+    public string CardType;
+    public int DamageMultiplier;
+    public string IconPath;
+}
---
+++ Assets/Scripts/Fgo/OnlyC#/FgoGameDataBase.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class FgoGameDataBase
+{
+    public string Id;
+}
