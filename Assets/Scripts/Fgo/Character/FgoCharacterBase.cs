0cfa3940e91ff71e3d7534a271ad31a4d3ec090a
blue_Air <blueair0379@gmail.com>
2026-05-31 22:47:38 +0900

    [스크립트 추가]캐릭터 베이스 및 적 컨트롤러 껍데기

---
+++ Assets/Scripts/Fgo/Character/FgoCharacterBase.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class FgoCharacterBase : MonoBehaviour
+{
+    [Header("--- 애니메이터 ---")]
+    [SerializeField] protected Animator _animator;
+
+    protected FgoCharacterData _statData;
+    public int currentHp;
+
+    public virtual void InitCharacter(FgoCharacterData data)
+    {
+        _statData = data;
+        currentHp = _statData.MaxHp;
+
+        Debug.Log($"[{_statData.Name}] 지정된 위치에 스폰 완료! (HP: {currentHp} / ATK: {_statData.Atk})");
+    }
+
+    public virtual void TakeDamage(int damage)
+    {
+        currentHp -= damage;
+        Debug.Log($"{gameObject.name} 피격! 데미지: {damage} (남은 HP: {currentHp})");
+
+        if (_animator != null)
+        {
+            _animator.SetTrigger("Hit");
+        }
+
+        if (currentHp <= 0)
+        {
+            Die();
+        }
+    }
+
+    protected virtual void Die()
+    {
+        Debug.Log($"{gameObject.name} 사망!");
+        if (_animator != null)
+        {
+            _animator.SetTrigger("Die");
+        }
+    }
+}
